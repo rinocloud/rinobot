22c1725270fe797dc3aaca250c301b96c6de552f
eoinmurray <amonecho1@gmail.com>
2016-08-31 12:42:30 +0200

    added docs

--- app/bot/readHistory.js
+++ app/bot/readHistory.js
@@ -1,6 +1,23 @@
 import fs from 'fs-extra'
 import _ from 'lodash'
 
+/*
+  Usage:
+
+    import readHistory from './readHistory'
+
+    readHistory({
+      historyFilePath: 'path/to/.rino/history.json',
+      targetFilepath: '/path/to/somefile.txt',
+    }, (err, historyOfFile) => {
+      if (err) throw err;
+
+      if (historyOfFile) {
+        console.log(historyOfFile)
+      }
+    })
+*/
+
 export default (opts, cb) => {
   const {
     historyFilePath,
--- app/bot/writeHistory.js
+++ app/bot/writeHistory.js
@@ -6,16 +6,17 @@ import fs from 'fs-extra'
     import writeHistory from './writeHistory'
 
     writeHistory({
-    historyFilePath: 'path/to/.rino/history.json',
-    targetFilepath: '/path/to/somefile.txt',
-    lastModified: 'ISO datetime string',
-    etag: '34h9834gh39u4h9384hgu3948hg',
-    completed: [ // list of completed task hashes
-      'as4390a439043433434v',
-      '4a34q34gq334gq34gq34w'
-    ]
-  })
-
+      historyFilePath: 'path/to/.rino/history.json',
+      targetFilepath: '/path/to/somefile.txt',
+      lastModified: 'ISO datetime string',
+      etag: '34h9834gh39u4h9384hgu3948hg',
+      completed: [ // list of completed task hashes
+        'as4390a439043433434v',
+        '4a34q34gq334gq34gq34w'
+      ]
+    }, (err) => {
+      if (err) throw err;
+    })
 */
 
 export default (opts, cb) => {
