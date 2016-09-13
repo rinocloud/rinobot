cda1612c1f7b74224043da28fb5faa1424ac2315
eoinmurray <amonecho1@gmail.com>
2016-09-13 20:29:48 +0100

    moved rpc and forkrpc mappings to  rpcMap

--- app/index.dev.js
+++ app/index.dev.js
@@ -6,17 +6,7 @@ import isDev from 'electron-is-dev'
 import _package from './package'
 import createMenu from './menu'
 import createRPC from './rpc'
-
-export const JSONError = function (error) {
-  this.name = error.name || ''
-  this.message = error.message || ''
-  this.stack = error.stack || ''
-  if (error.code) this.code = error.code
-  if (error.errno) this.errno = error.errno || ''
-  if (error.syscall) this.syscall = error.syscall || ''
-  if (error.path) this.path = error.path || ''
-}
-JSONError.prototype = Error.prototype;
+import rpcMap from './rpcMap'
 
 const isOSX = process.platform === 'darwin'
 
@@ -33,31 +23,7 @@ const createWindow = (app, sentry) => { // eslint-disable-line
   const rpc = createRPC(win)
   const { child, forkRpc } = createBot()
 
-  rpc.on('watch', args => forkRpc.emit('watch', args))
-  rpc.on('unwatch', args => forkRpc.emit('unwatch', args))
-  rpc.on('unwatch all', args => forkRpc.emit('unwatch all', args))
-
-  forkRpc.on('ready', () => rpc.emit('child process ready'))
-
-  forkRpc.on('watcher ready', args => rpc.emit('watcher ready', args))
-  forkRpc.on('watcher started', args => rpc.emit('watcher started', args))
-  forkRpc.on('watcher set total files', args => rpc.emit('watcher set total files', args))
-  forkRpc.on('watcher set processed files', args => rpc.emit('watcher set processed files', args))
-
-  forkRpc.on('set history', args => rpc.emit('set history', args))
-  forkRpc.on('task started', args => rpc.emit('task started', args))
-  forkRpc.on('task log', args => rpc.emit('task log', args))
-  forkRpc.on('task complete', args => rpc.emit('task complete', args))
-  forkRpc.on('task error', args => {
-    sentry.captureException(new JSONError(args.error))
-    rpc.emit('task error', args)
-  })
-  forkRpc.on('task ignore', args => rpc.emit('task ignore', args))
-
-  forkRpc.on('unexpected error', error => {
-    rpc.emit('unexpected error', error)
-    sentry.captureException(new JSONError(error))
-  })
+  rpcMap(rpc, forkRpc, sentry)
 
   rpc.on('init', () => {
     win.show()
